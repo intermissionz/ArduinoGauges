7a7e517f3a3dfe664d4ad2026b0f18e9c392bff2
intermissionz <38229033+intermissionz@users.noreply.github.com>
2018-05-09 13:12:41 -0400

    Adding PIDTest sketch

---
+++ PIDTest/PIDTest.ino
@@ -0,0 +1,28 @@
+#include <CAN.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("CAN OBD-II PID test");
+  CAN.begin(500E3));
+  CAN.filter(0x7e0);
+}
+
+void loop() {
+  CAN.beginPacket(0x7df, 8);
+  CAN.write(0x02); // number of additional bytes
+  CAN.write(0x21); // OBD mode
+  CAN.write(0x01); // PID
+  CAN.endPacket();
+  Serial.println("packet sent");
+  delay(10);
+  Serial.println(CAN.parsePacket());
+  do {
+    Serial.println(CAN.read());
+  } while (CAN.available()); //CAN.parsePacket() == 0 || CAN.read() < 3 || CAN.read() != 0x41 || CAN.read() != 0x33);
+    
+  /*float value = (CAN.read() - 40) * 1.8 + 32;
+  Serial.print("Value = ");
+  Serial.println(value);*/
+
+  delay(2000);
+}
